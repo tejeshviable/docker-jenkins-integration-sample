a686a0a7e81c0716bd65706a7d24cd83da829d0d
tejeshviable <roy.tejashvi@gmail.com>
2020-01-04 12:51:46 +0530

    second commit

---
+++ src/main/java/HelloDockerJenkinsTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created by tejeshvi.roy on 04-01-2020.
+ */
+public class HelloDockerJenkinsTest {
+
+    public static void main(String[] args) {
+        System.out.println("HelloDockerJenkinsTest");
+    }
+}
